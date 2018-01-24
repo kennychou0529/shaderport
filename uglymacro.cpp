2f6cedad1886673741ad3376858d6ca1f1d0799a
lightbits <simenh.jobb@gmail.com>
2018-01-24 13:31:31 +0100

    unused file

--- uglymacro.cpp
+++
@@ -1,6 +0,0 @@
-#define Triggered(EVENT, DURATION)                      \
-    static double tdb_timer_##__LINE__ = 0.0f;          \
-    if (EVENT) tdb_timer_##__LINE__ = glfwGetTime();    \
-    if (glfwGetTime() - tdb_timer_##__LINE__ < DURATION)
-
-

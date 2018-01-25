829d9cbf69a40ce08e3f6a1d45f051f919778694
lightbits <simenh.jobb@gmail.com>
2018-01-25 17:18:02 +0100

    move in macros

--- macro_one_time_event.h
+++
@@ -1,5 +0,0 @@
-#define OneTimeEvent(VAR, EVENT)                     \
-    static bool VAR##_was_active = (EVENT);          \
-    bool VAR##_is_active = (EVENT);                  \
-    bool VAR = VAR##_is_active && !VAR##_was_active; \
-    VAR##_was_active = VAR##_is_active;
--- main.cpp
+++ main.cpp
@@ -177,6 +177,18 @@ void SetWindowSizeDialog(bool *escape_eaten, GLFWwindow *window, frame_input_t i
     }
 }
 
+#define Triggered(EVENT, DURATION)                      \
+    static double tdb_timer_##__LINE__ = 0.0f;          \
+    if (EVENT) tdb_timer_##__LINE__ = glfwGetTime();    \
+    if (glfwGetTime() - tdb_timer_##__LINE__ < DURATION)
+
+#define OneTimeEvent(VAR, EVENT)                     \
+    static bool VAR##_was_active = (EVENT);          \
+    bool VAR##_is_active = (EVENT);                  \
+    bool VAR = VAR##_is_active && !VAR##_was_active; \
+    VAR##_was_active = VAR##_is_active;
+
+
 int main(int argc, char **argv)
 {
     // assert(false && "See comment above");

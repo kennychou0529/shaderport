2beedc9f05ec21e66477b9bdafb80d0944f5dc59
lightbits <simenh.jobb@gmail.com>
2018-01-27 19:27:14 +0100

    settings file

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
 
 #include "framegrab.cpp"
 #include "perframe.cpp"
+#include "settings.cpp"
 
 #define GLFW_EXPOSE_NATIVE_WIN32
 #define GLFW_EXPOSE_NATIVE_WGL
@@ -236,65 +237,6 @@ script_loop_t LoadScript()
     }
     tcc_delete(s);
     return ScriptLoop;
-}
-
-struct settings_t
-{
-    bool never_ask_on_exit;
-    int window_x;
-    int window_y;
-    int window_w;
-    int window_h;
-};
-
-void SaveSettings(settings_t s, const char *filename)
-{
-    FILE *f = fopen(filename, "wb");
-    if (!f)
-    {
-        printf("Failed to save settings file '%s'\n", filename);
-        return;
-    }
-    fprintf(f, "Pos=%d,%d\n", s.window_x, s.window_y);
-    fprintf(f, "Size=%d,%d\n", s.window_w, s.window_h);
-    fprintf(f, "NeverAskOnExit=%d\n", s.never_ask_on_exit);
-    fclose(f);
-}
-
-settings_t LoadSettingsOrDefault(const char *filename)
-{
-    settings_t settings = {0};
-    settings.window_x = -1;
-    settings.window_y = -1;
-    settings.window_w = 1000;
-    settings.window_h = 600;
-    settings.never_ask_on_exit = false;
-
-    FILE *f = fopen(filename, "rb");
-    if (!f)
-    {
-        return settings;
-    }
-    int file_size;
-    if (fseek(f, 0, SEEK_END) || (file_size = (int)ftell(f)) == -1 || fseek(f, 0, SEEK_SET))
-    {
-        fclose(f);
-        return settings;
-    }
-    char *line = (char*)malloc(file_size);
-
-    while (fscanf(f, "%s", line) == 1) // todo: more robust line-by-line reading?
-    {
-        // very rudimentary parser
-        int x,y;
-        int i;
-        if (sscanf(line, "Pos=%d,%d", &x, &y) == 2)          { settings.window_x = x; settings.window_y = y; }
-        else if (sscanf(line, "Size=%d,%d", &x, &y) == 2)    { settings.window_w = x; settings.window_h = y; }
-        else if (sscanf(line, "NeverAskOnExit=%d", &i) == 1) { settings.never_ask_on_exit = (i != 0); }
-    }
-
-    free(line);
-    return settings;
 }
 
 int main(int argc, char **argv)

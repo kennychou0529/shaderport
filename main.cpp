a6b28e995aa0534e63b87b3763dd25cb119a89b0
lightbits <simenh.jobb@gmail.com>
2018-01-28 15:10:20 +0100

    removed perframe.cpp

--- main.cpp
+++ main.cpp
@@ -243,10 +243,50 @@ script_loop_t LoadScript()
     return ScriptLoop;
 }
 
+void ResetGLState(frame_input_t input)
+{
+    glUseProgram(0);
+
+    glEnable(GL_BLEND);
+    glBlendEquation(GL_FUNC_ADD);
+    glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE);
+
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_SCISSOR_TEST);
+    glDisable(GL_TEXTURE_2D);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    glViewport(0, 0, (GLsizei)input.framebuffer_w, (GLsizei)input.framebuffer_h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0f, +1.0f, -1.0f, +1.0f, -1.0f, +1.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glLineWidth(1.0f);
+    glPointSize(1.0f);
+}
+
+static frame_input_t frame_input = {0};
+void BeforeUpdateAndDraw(frame_input_t input)
+{
+    frame_input = input;
+
+    ResetGLState(input);
+    glClearColor(0.1f, 0.12f, 0.15f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    vdbBeforeUpdateAndDraw(input);
+}
+
+void AfterUpdateAndDraw(frame_input_t input)
+{
+    ResetGLState(input);
+}
+
 int main(int argc, char **argv)
 {
-    // assert(false && "See comment above");
-
     unsigned int command_buffer_allocation_size = 1024*1024*10;
     if (!AllocateCommandBuffers(command_buffer_allocation_size))
     {
